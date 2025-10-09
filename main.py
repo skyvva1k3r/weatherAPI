fc0e0b0d00d662a012f9d26128a03eb71de1c2c1
skyLaptop <antargtig@inbox.ru>
2025-10-09 15:20:00 +0300

    nothing important

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import json
 from dotenv import load_dotenv
 import os
+import time
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
@@ -37,13 +38,17 @@ r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 key = f"{API_KEY}"
 
 city = "Moscow"
-time = 0
+date = "2025-10-09"
+cur = f"{time.strftime('%Y-%m-%d', time.localtime())}"
 
-def get_data(city, time, key):
-        if r.exists(f"{city}_{time}") == 0:
+
+def get_data(city, date, cur, key):
+        if r.exists(f"{city}_{date}_{cur}") == 0:
             print("No data in Redis, fetching...")
-            r.set(f"{city}_{time}", json.dumps(requests.get(f"http://api.weatherapi.com/v1/forecast.json?key={key}&q={city}&days={time}&aqi=no&alerts=no").json()))
+            r.set(f"{city}_{date}_{cur}", json.dumps(requests.get(f"http://api.weatherapi.com/v1/forecast.json?key={key}&q={city}&dt={date}&aqi=no&alerts=no").json()))
         else: 
             print("Data in Redis, fetching...")
+        print(json.loads(r.get(f"{city}_{date}_{cur}"))["forecast"]["forecastday"][0]["day"]["maxtemp_c"])
 
-get_data(city, time, key)
+
+get_data(city, date, cur, key)
