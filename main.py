0177cf09fcbd844bf9ba4c68f51d0c769840cbc0
skyLaptop <antargtig@inbox.ru>
2025-10-10 16:16:08 +0300

    working on returning stuff

--- main.py
+++ main.py
@@ -41,26 +41,43 @@ key = f"{API_KEY}"
 @app.route('/weather')
 def weather():
     city = request.args.get('city')
-    return jsonify(get_data(city))
+    days = request.args.get('days')
+    if days is None:
+        return jsonify({"error": "Days parameter is required"}), 400
+    days = int(days)
+    if days > 14:
+        return jsonify({"error": "Days parameter must be less than 14"}), 400
+    if days < 1:
+        return jsonify({"error": "Days parameter must be greater than 0"}), 400
+    if city is None:
+        return jsonify({"error": "City parameter is required"}), 400
+    result = get_data(city.lower(), days)
+    if "error" in result: 
+        return jsonify(result), 404
+    return jsonify(result), 200
 
-def get_data(city):
+def get_data(city, days):
         cur = f"{time.strftime('%Y-%m-%d', time.localtime())}"
         req = ["tempmax", "tempmin", "feelslike", "pressure", "windspeed", "visibility"]
-        res = []
+        res = dict()
         if r.exists(f"{city}_{cur}") == 0:
             print("No data in Redis, fetching...")
-            r.set(f"{city}_{cur}", json.dumps(requests.get(f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}?key={key}").json()))
+            response = requests.get(f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{city}?key={key}")
+            if response.status_code != 200:
+                return {"error": "City not found"}
+            r.set(f"{city}_{cur}", json.dumps(response.json()))
         else: 
             print("Data in Redis, fetching...")
-        for i in range(14):
-            res.append(itemgetter(*req)(json.loads(r.get(f"{city}_{cur}"))["days"][i]))
-            res[i] = list(res[i])
-            res[i][0] = round(((res[i][0] -32)*5)/9, 1)
-            res[i][1] = round(((res[i][1] -32)*5)/9, 1)
-            res[i][2] = round(((res[i][2] -32)*5)/9, 1)
-            res[i] = tuple(res[i])
+        temp = json.loads(r.get(f"{city}_{cur}"))["days"]
+        for i in range(days):
+            res.update({i : {}})
+            for j in req:
+                res[i].update({j : temp[i][j]})
+                if j == "tempmax" or j == "tempmin" or j == "feelslike":
+                    res[i][j] = round(((res[i][j] -32)*5)/9, 1)
         return res
 
+# get_data("")
 
 if __name__ == '__main__':
     app.run()
